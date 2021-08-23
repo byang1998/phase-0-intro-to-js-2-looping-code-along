01d930f52c4bb448bea0037aedb2b42f10be3a18
Ben Yang <benyang2020@gmail.com>
2021-08-23 03:07:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+//const names = ["Lisa", "Kaitlin", "Jan"]; 
+function writeCards(names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+      
+    }
+    return newArray;
+}
+
+function countDown () {
+    let i=10;
+    while (i > -1) {
+        console.log(i--);
+        
+    }
+    return i;
+}
